8866426c7a45b05283eb1298636554ae2439d885
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-11-10 01:04:39 +0100

    kreirana je WebShopCentar aplikacija i odradjen je prvi komit

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase; // Koristi se za resetovanje baze između testova
+
+    /** @test */
+    public function user_can_be_created()
+    {
+        $user = User::create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => bcrypt('password123'),
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'john@example.com'
+        ]);
+    }
+}
