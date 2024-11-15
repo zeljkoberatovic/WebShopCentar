6918cea99335390d3c3986209c0ff9264e22e8a0
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-11-15 11:02:03 +0100

    Dodat hero dio(test verzija) na home.pages

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,28 @@
 @extends('layouts.app')
 
-  
+@section('title', 'Pocetna')
+
+@section('content')  
+
+<section class="hero">
+    <div class="hero__content">
+        <h1>Tvoj Web Shop,tvoj biznis</h1>
+        <p>Idealno mesto za tvoj novi početak – iznajmi prodavnicu i razvij svoj biznis vec danas!</p>
+        <a href="#shop" class="hero__button">Počni sada</a>
+    </div>
+    <div class="hero__image">
+        <picture>
+            <source srcset="{{ asset('images/hero-img.webp') }}" type="image/webp">
+            <img src="{{ asset('images/hero-img.jpg') }}" alt="Opis slike">
+        </picture>
+        
+    </div>
+    
+</section>
+
+
+
+@endsection
    
 
 
