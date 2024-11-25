9c9c18456435ac4e62d8b9bfa743445f05a0ab8a
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-11-25 17:39:28 +0100

    Kreiran StoreControler i ruta za njega

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\StoreController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/stores', [StoreController::class, 'index'])->name('stores.index');
 
 
 
