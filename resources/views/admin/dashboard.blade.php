ffa7308a0e88727d5e1729f97be97a73b33efd92
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-12-30 11:43:49 +0100

    Kreirani folderi u views admin i user i u njih kreirani fajlovi dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Admin Dashboard</h1>
+    <p>
+        Dobrodošli na našu stranicu! Mi smo kompanija posvećena pružanju najboljih rešenja 
+        za kreiranje i iznajmljivanje web prodavnica. Naš cilj je omogućiti vašem poslovanju 
+        online prisustvo koje zaslužuje.
+    </p>
+    
+</div>
+@endsection
